5d0fde40e273a288a130204d9f749f549c6c4bb9
Arthur Kabera <arthurkabera@gmail.com>
2024-10-23 10:04:57 +0300

    added homepage

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ app.get('/api/hello', (req, res) => {
     res.json({ message: 'Hello, world!' });
 });
 
+// Add a new route to serve the HTML page
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
